cedf357f3e1d5d6834ddc119c8167ff939db82e9
Mohammad Saddam Ali <mohdsaddamali002@gmail.com>
2024-08-06 20:25:57 +0545

    operators

---
+++ assignment.py
@@ -0,0 +1,42 @@
+# ARTHEMETIC OPERATOR
+a = 10
+b = 5
+
+c = a+b 
+d = a-b  
+e = a*b 
+f = a%b  
+g = a/b
+h = a//b
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+
+# COMPARISON OPERATORS
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+
+# LOGICAL OPERATORS
+print(True and True)
+print(True and False)
+print(False and False)
+print(True or True)
+print(True or False)
+print(False or False)
+print(not b)
+
+# ASSIGNMENT OPERATORS
+
+a = b
+a += b
+a -= b
+print(a)
+
